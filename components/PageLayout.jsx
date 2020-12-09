14b878a9eaea5b01a556526aca428878d890f60a
Junmin De Real <junmindereal@gmail.com>
2020-12-09 21:58:50 +0800

    SEO update on PageLayout

--- components/PageLayout.jsx
+++ components/PageLayout.jsx
@@ -2,15 +2,22 @@ import Head from 'next/head'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 
-const PageLayout = ({ children, pageTitle, description }) => {
+const PageLayout = ({ children, pageTitle, description, slug }) => {
+  const siteName = 'LDS Wiki'
+  const url = 'https://ldswiki.org/'
   return (
     <>
       <Head>
-        <title>{pageTitle}</title>
         <link rel='icon' href='/favicon.ico' />
         <meta name='viewport' content='width=device-width' />
         <meta charSet='utf-8' />
         <meta name='description' content={description} />
+
+        <meta property='og:url' content={slug ? url + slug : url} key='ogurl' />
+        <meta property='og:site_name' content={siteName} key='ogsitename' />
+        <meta property='og:title' content={pageTitle} key='ogtitle' />
+        <meta property='og:description' content={description} key='ogdesc' />
+        <title>{pageTitle}</title>
       </Head>
       <div className='max-w-2xl px-4 mx-auto antialiased xl:px-0'>
         <Navbar />
