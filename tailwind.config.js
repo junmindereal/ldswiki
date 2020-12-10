720eae4236371de46cfc02847b00e38e5b18ce61
Junmin De Real <junmindereal@gmail.com>
2020-12-10 18:31:19 +0800

    Updated dependencie to fix tailwindcss issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: ['./pages/*.js', './components/*.js'],
+  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
     colors: {
